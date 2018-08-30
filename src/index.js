530840295c56d713d545b79fca4777cd887a9b34
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-08-30 16:21:13 +0200

    with video detail and youtube player

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import YTSearch from 'youtube-api-search';
+import VideoDetail from './components/video_detail';
 
-const API_KEY=
+const API_KEY=;
 
 
 
@@ -27,6 +28,7 @@ class App extends Component{
     return(
       <div>
       <SearchBar />
+      <VideoDetail video={this.state.videos[0]}/>
       <VideoList videos={this.state.videos} />
       </div>
       );
