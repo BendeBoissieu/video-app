bd6dcb70faaadb3ce277f7fa4084bebdc94f2047
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-08-30 18:13:12 +0200

    callback to see other videos

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
   //old syntax with (props) new on ES6 only ({video})
   //const video = props.video;
   const imageUrl = video.snippet.thumbnails.default.url;
   console.log(video.snippet.title);
   return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img media-object  src={imageUrl}/>
